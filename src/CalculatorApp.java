ad648320dbc3ef0d6888c034829e36b281b16409
mrajde01 <meet@doprocess.com>
2021-01-31 11:39:12 -0500

    Formatting and adding comments

--- src/CalculatorApp.java
+++ src/CalculatorApp.java
@@ -5,52 +5,46 @@ public class CalculatorApp {
 	Scanner scan = new Scanner(System.in);
 
 	public static void main(String[] args) {
-
 		System.out.println("Calculator :");
 		CalculatorApp obj = new CalculatorApp();
 		obj.calculate();
 	}
 
+	// Takes input and does calculation
 	public void calculate() {
 		try {	
 			System.out.println("Enter number 1");
 			int num1 = scan.nextInt();
-	
 			System.out.println("Enter number 2");
 			int num2 = scan.nextInt();
-	
 			System.out.println("Enter operand");
 			String o = scan.next();
-	
+			
 			int ans = 0;
 			if (o.equals("+"))
 				ans = num1 + num2;
-	
 			else if (o.equals("-")) {
 				ans = (num1 > num2) ? num1 - num2 : num2 - num1;
 			}
-	
 			else if (o.equals("*"))
 				ans = num1 * num2;
-	
 			else if (o.equals("/"))
 				ans = num1 / num2;
-	
 			else if (o.equals("%"))
 				ans = num1 % num2;
-			
 			else
+				// Exception for operand
 				throw new Exception ("The operand must be from + - * / %. Try again.");
 	
 			System.out.println("Answer is " + ans);
 		}
-		
+		// Exception for inputs
 		catch(Exception e) {
 			System.out.println(e);
 		}
-
 		CalculatorApp obj = new CalculatorApp();
 		boolean result = obj.movingForward();
+		
 		if (result)
 			obj.calculate();
 		else {
@@ -59,6 +53,7 @@ public class CalculatorApp {
 		}
 	}
 
+	// Check if user wants to try again or not
 	public boolean movingForward() {
 		System.out.println("Do you want to continue? y/n :");
 		CalculatorApp obj = new CalculatorApp();
@@ -68,13 +63,13 @@ public class CalculatorApp {
 		if (moveForward.equals("y") || moveForward.equals("Y")) {
 			result = true;
 			System.out.println("--------------------------------");
-		} else if (moveForward.equals("n") || moveForward.equals("N"))
+		} 
+		else if (moveForward.equals("n") || moveForward.equals("N"))
 			result = false;
 		else {
 			System.out.println("Invalid entry, re-enter!!");
 			obj.movingForward();
 		}
-
 		return result;
 	}
 
